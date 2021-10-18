f7f8310031b839891db430fa318aee7c5322c6ce
zhangsan <zhangsan@qq.com>
2021-10-18 23:10:49 +0800

    创建数据并编写第一个接口‘ ’

---
+++ server/__init__.py
@@ -0,0 +1,5 @@
+import datetime
+
+n = datetime.datetime.now()
+r = datetime.datetime.strftime(n, "%Y-%m-%d")
+print(r)
--- server/app.py
+++ server/app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+import datetime
+
+from flask import Flask,make_response
 from flask_sqlalchemy import SQLAlchemy
-import os
+import os,json,click
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(app.root_path,'taskmgr.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 db = SQLAlchemy(app)
@@ -16,28 +19,59 @@ class Projects(db.Model):
 # 任务表
 class Tasks(db.Model):
     id = db.Column(db.Integer,primary_key=True)
-    title = db.Column(db.String(60))
+    title = db.Column(db.String(80))
     date = db.Column(db.DateTime)
-    condition = db.Column(db.String(10))
-    pid = db.Column(db.Integer)
+    condition = db.Column(db.String(10)) # 任务的状态
+    pid = db.Column(db.Integer) # 项目表的id
+    sort = db.Column(db.Integer)  # 记录用户的排序
 
-projects=[
-    {'name':'项目1'},
-    {'name':'项目2'},
-    {'name':'项目3'},
-],
-tasks=[
-    {'title':'逛公园','date':'10月13日','condition':'todo','pid':1},
-    {'title':'完成游戏','date':'10月12日','condition':'todo','pid':1},
-    {'title':'吃饭','date':'10月15日','condition':'done','pid':1},
-    {'title':'上王者','date':'10月16日','condition':'done','pid':1},
-    {'title': '撸猫', 'date': '10月13日', 'condition': 'todo', 'pid': 2},
-    {'title': '看电视剧', 'date': '10月12日', 'condition': 'todo', 'pid': 2},
-    {'title': '逛街', 'date': '10月15日', 'condition': 'done', 'pid': 2},
-    {'title': '去XX景区', 'date': '10月16日', 'condition': 'done', 'pid': 3},
-]
+# 创建初始化数据
+@app.cli.command()
+def forge():
+    # 先删除之前的记录,再创建数据库表
+    db.drop_all()
+    db.create_all()
+    # 向项目表中插入数据
+    projects=[
+        {'name':'项目1'},
+        {'name':'项目2'},
+        {'name':'项目3'},
+    ]
+    for p in projects:
+        project = Projects(name=p['name'])
+        db.session.add(project)
+    # 向任务表中插入数据
+    n = datetime.datetime.now()
+    tasks=[
+        {'title':'逛公园','date':n,'condition':'todo','pid':1,'sort':1},
+        {'title':'完成游戏','date':n,'condition':'todo','pid':1,'sort':2},
+        {'title':'吃饭','date':n,'condition':'done','pid':1,'sort':3},
+        {'title':'上王者','date':n,'condition':'done','pid':1,'sort':4},
+        {'title': '撸猫', 'date': n, 'condition': 'todo', 'pid': 2,'sort':5},
+        {'title': '看电视剧', 'date': n, 'condition': 'todo', 'pid': 2,'sort':6},
+        {'title': '逛街', 'date': n, 'condition': 'done', 'pid': 2,'sort':7},
+        {'title': '去XX景区', 'date': n, 'condition': 'done', 'pid': 3,'sort':8},
+    ]
+    for t in tasks:
+        task = Tasks(title=t['title'],date=t['date'],condition=t['condition'],pid=t['pid'],sort=t['sort'])
+        db.session.add(task)
+    db.session.commit()
+    click.echo('Done')
+
 
 @app.route('/')
 def hello():
-    return tasks 
-
+    tasks = []
+    ts = Tasks.query.filter_by(pid=2).all()
+    for t in ts:
+        d = datetime.datetime.strftime(t.date, "%Y-%m-%d")
+        r = {'title':t.title,'date':d,'condition':t.condition,'pid':t.pid}
+        tasks.append(r)
+    print(tasks)
+    res = make_response(json.dumps(tasks))
+    res.headers['Content-Type'] = 'application/json; charset=utf-8'
+    res.status = '200'
+    return res
+#
+# if __name__ == '__main__':
+#     app.run()
